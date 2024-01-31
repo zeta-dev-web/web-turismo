d35c6cd4879982bbb0b09c3017f27997449f2a98
Leonel Sanagua <leonel.sanagua@gmail.com>
2024-01-31 13:27:38 -0300

    Creacion de navbar

--- src/components/NavbarApp.jsx
+++ src/components/NavbarApp.jsx
@@ -1,22 +1,27 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Navbar, Nav } from "rsuite";
+import HomeIcon from "@rsuite/icons/legacy/Home";
+import CogIcon from "@rsuite/icons/legacy/Cog";
 
-const NavbarApp = () => {
-  return (
-    <>
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
-  );
-};
+const NavbarApp = () => (
+  <Navbar>
+    <Navbar.Brand href="#">RSUITE</Navbar.Brand>
+    <Nav>
+      <Nav.Item icon={<HomeIcon />}>Home</Nav.Item>
+      <Nav.Item>News</Nav.Item>
+      <Nav.Item>Products</Nav.Item>
+      <Nav.Menu title="About">
+        <Nav.Item>Company</Nav.Item>
+        <Nav.Item>Team</Nav.Item>
+        <Nav.Menu title="Contact">
+          <Nav.Item>Via email</Nav.Item>
+          <Nav.Item>Via telephone</Nav.Item>
+        </Nav.Menu>
+      </Nav.Menu>
+    </Nav>
+    <Nav pullRight>
+      <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
+    </Nav>
+  </Navbar>
+);
 
 export default NavbarApp;
