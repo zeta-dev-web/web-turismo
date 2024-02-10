19cb9a5ff37c58faff377d3fd8ac713ac9ef8ddc
zeta-dev <nvreparaciones@gmail.com>
2024-02-10 15:53:49 -0300

    correccion de texto de tarjeta de informacion de trancas, se agrega imagen

--- src/components/CardTextApp.jsx
+++ src/components/CardTextApp.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const CardTextApp = () => {
   return (
     <div className='ms-3 me-2 text-center'>
-        <img src="https://i.ibb.co/vJjg46N/trancas.jpg" alt="trancas" className='img-fluid'/>
-      <h6>Trancas es una ciudad con un enorme peso histórico. El turista conocerá un pueblo que fue centro estratégico para la defensa de todo el norte en la época de la Independencia. 
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Trancas_%28Provincia_de_Tucum%C3%A1n_-_Argentina%29.svg/212px-Trancas_%28Provincia_de_Tucum%C3%A1n_-_Argentina%29.svg.png" alt="trancas" className='img-fluid'/>
+      <h6 className='mt-2 text-start'>Trancas es una ciudad con un enorme peso histórico. El turista conocerá un pueblo que fue centro estratégico para la defensa de todo el norte en la época de la Independencia. 
 En un principio, la población se instaló en lo que hoy se conoce como “Villa Vieja de Trancas” pero en 1900, con la llegada del ferrocarril, debieron moverse a su actual emplazamiento donde se establecieron definitivamente. En la actual ciudad, pueden encontrarse opciones de alojamiento y gastronomía.
 Un imperdible de la ciudad, es la Fiesta Provincial del Caballo, realizada cada año en el mes de octubre. El visitante podrá ver desfiles de tropillas, doma, elección del mejor gaucho y la mejor donosa, juegos de riendas, entre otros. Además, presenciar espectáculos de artistas folklóricos provinciales y nacionales.</h6>
     </div>
