1d30ae2807b0219570f8dd85cb1183f3592e66d1
zeta-dev <nvreparaciones@gmail.com>
2024-02-13 13:42:20 -0300

    se agregan las pages de Lugares Turisticos, Gastronomia

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { React, useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomeScreen from "./views/HomeScreen";
+import ContactScreen from "./views/ContactScreen";
+import FoodScreen from "./views/FoodScreen";
+import ExploreScreen from "./views/ExploreScreen";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
+        <Route path="LugaresTuristicos" element={<ExploreScreen />} />
+        <Route path="Gastronomia" element={<FoodScreen />} />
+        <Route path="Contacto" element={<ContactScreen />} />
+        {/* <Route path="/" element={<FoodScreen />} /> */}
       </Routes>
     </BrowserRouter>
   );
--- src/views/FoodScreen.jsx
+++ src/views/FoodScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ContactScreen = () => {
+const FoodScreen = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const ContactScreen = () => {
   )
 }
 
-export default ContactScreen
+export default FoodScreen
