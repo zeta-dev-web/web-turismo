46438fa3e7adcf21f0850c8483969f4c38ad47a7
zeta-dev <nvreparaciones@gmail.com>
2024-02-13 14:30:45 -0300

    se corrige nombre de sitio web, idioma e icono

--- src/components/NavbarApp.jsx
+++ src/components/NavbarApp.jsx
@@ -6,7 +6,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 const NavbarApp = () => (
   <Navbar expand="lg" className="bg-body-tertiary">
   <Container>
-    <Navbar.Brand href="#home">Turismo Trancas</Navbar.Brand>
+    <Navbar.Brand href="/">Turismo Trancas</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
