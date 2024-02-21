5bde2abda17d7c7315d4281aa1898932ed921cce
zeta-dev <nvreparaciones@gmail.com>
2024-02-21 13:33:01 -0300

    se corrigen textos y rutas de enlaces

--- src/components/CardTextApp.jsx
+++ src/components/CardTextApp.jsx
@@ -4,9 +4,13 @@ const CardTextApp = () => {
   return (
     <div className='ms-3 me-2 text-center'>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Trancas_%28Provincia_de_Tucum%C3%A1n_-_Argentina%29.svg/212px-Trancas_%28Provincia_de_Tucum%C3%A1n_-_Argentina%29.svg.png" alt="trancas" className='img-fluid'/>
-      <h6 className='mt-2 text-start'>Trancas es una ciudad con un enorme peso histórico. El turista conocerá un pueblo que fue centro estratégico para la defensa de todo el norte en la época de la Independencia. 
-En un principio, la población se instaló en lo que hoy se conoce como “Villa Vieja de Trancas” pero en 1900, con la llegada del ferrocarril, debieron moverse a su actual emplazamiento donde se establecieron definitivamente. En la actual ciudad, pueden encontrarse opciones de alojamiento y gastronomía.
-Un imperdible de la ciudad, es la Fiesta Provincial del Caballo, realizada cada año en el mes de octubre. El visitante podrá ver desfiles de tropillas, doma, elección del mejor gaucho y la mejor donosa, juegos de riendas, entre otros. Además, presenciar espectáculos de artistas folklóricos provinciales y nacionales.</h6>
+      <h6 className='mt-2 text-start'>Trancas, en el corazón de las Cumbres Calchaquíes, te invita a descubrir un remanso de paz y belleza natural. Este destino te ofrece una experiencia única para conectar con la naturaleza, la historia y la cultura.<br/>
+
+Déjate sorprender por las vistas panorámicas, los ríos y su gastronomía. Practica senderismo, cabalgatas, pesca o simplemente relájate en la tranquilidad del pueblo. Recorre la Villa Vieja de Trancas, conoce la Iglesia de Nuestra Señora del Rosario y participa de las tradiciones locales.<br/>
+
+No te pierdas el Festival del Caballo, un evento único que reúne a jinetes de toda la región. Celebra las fiestas patronales con música, bailes y comidas típicas. Llévate un recuerdo único hecho a mano por los artesanos locales.<br/>
+
+<h5>Trancas te espera para que vivas una experiencia inolvidable. Ven a descubrir este paraíso escondido en Tucumán.</h5></h6>
     </div>
   )
 }
