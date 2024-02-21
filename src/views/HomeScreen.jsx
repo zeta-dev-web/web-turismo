e305c60539288753cbd876e1818516585a39ea43
zeta-dev <nvreparaciones@gmail.com>
2024-02-21 16:12:16 -0300

    se cambia icono de card de pagina pricipal de eventos locales

--- src/views/HomeScreen.jsx
+++ src/views/HomeScreen.jsx
@@ -8,12 +8,13 @@ import CardTextApp from "../components/CardTextApp";
 import { FaMapSigns } from "react-icons/fa";
 import { MdFoodBank } from "react-icons/md";
 import { SiGooglemaps } from "react-icons/si";
+import { TfiAgenda } from "react-icons/tfi";
 
 const HomeScreen = () => {
   const cardsData = [
     { titulo: 'Lugares Turísticos', contenido: 'Conoce los lugares historicos...', icono:<FaMapSigns size={35}/>, boton:"Ver más...", ruta: "lugaresturisticos"},
     { titulo: 'Gastronomía', contenido: 'Disfruta de los mejores platos...', icono: <MdFoodBank size={35}/>, boton:"Ver más...", ruta: "/gastronomia"},
-    { titulo: 'Eventos Locales', contenido: 'Conoce las fiestas locales...', icono: <SiGooglemaps size={35}/>,boton:"Ver más...",ruta:"/eventoslocales" },
+    { titulo: 'Eventos Locales', contenido: 'Conoce las fiestas locales...', icono: <TfiAgenda- size={35}/>,boton:"Ver más...",ruta:"/eventoslocales" },
     { titulo: 'Ubicación', contenido: 'Como llegar a Trancas...', icono: <SiGooglemaps size={35}/>,boton:"Abrir en Google Maps",ruta:"https://maps.app.goo.gl/YCVdmd4VJHox8xyC6" },
   ];
   return (
