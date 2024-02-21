3316b9b503a84ca8fcfd754ba3a78dfb3f37a8e9
zeta-dev <nvreparaciones@gmail.com>
2024-02-21 18:37:09 -0300

    se corrige footer, margenes y formatos

--- src/components/FooterApp.jsx
+++ src/components/FooterApp.jsx
@@ -1,39 +1,35 @@
 import "../css/FooterApp.css";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
 
 function FooterApp() {
   return (
     <div className="contenedor2 d-flex flex-column">
-      <footer className="py-3 mt-auto d-flex flex-wrap justify-content-between align-items-center border-top color w-100">
-        <div className="col-md-4 d-flex align-items-center text-center">
-          <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-            <svg className="bi" width="30" height="24">
-              <use xlinkHref="#bootstrap"></use>
-            </svg>
-          </a>
-          <span className="mb-3 mb-md-0 text-white">ZetaDev & Leonel © 2023 Company, Inc</span>
-        </div>
-
-        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <footer className="py-1 mt-auto d-flex flex-wrap justify-content-between align-items-center border-top color w-100">
+      <div className="col-md-3 d-none d-md-flex align-items-center text-center mx-auto">
+  <span className="mb-3 mb-md-0 text-white mt-3 mb-2">ZetaDev & Leonel© 2024</span>
+</div>
+<div className="col-md-3 d-none d-md-flex align-items-center text-center mx-auto">
+  <h6 className="ms-5 mb-3 mb-md-0 text-white mt-2">Municipalidad de Trancas 2024</h6>
+</div>
+        <ul className="nav col-md-3 justify-content-end d-flex mx-auto">
           <li className="ms-3">
-            <a className="text-body-secondary" href="#">
-              <svg className="bi" width="24" height="24">
-                <use xlinkHref="#twitter"></use>
-              </svg>
-            </a>
+          <Link to="#">
+              <FaFacebookSquare size="30" className="text-white" />
+            </Link>
           </li>
           <li className="ms-3">
-            <a className="text-body-secondary" href="#">
-              <svg className="bi" width="24" height="24">
-                <use xlinkHref="#instagram"></use>
-              </svg>
-            </a>
+          <Link to="#">
+              <FaSquareInstagram size="30" className="text-white" />
+            </Link>
           </li>
           <li className="ms-3">
-            <a className="text-body-secondary" href="#">
-              <svg className="bi" width="24" height="24">
-                <use xlinkHref="#facebook"></use>
-              </svg>
-            </a>
+          <Link to="#">
+              <FaTwitterSquare size="30" className="text-white" />
+            </Link>
           </li>
         </ul>
       </footer>
