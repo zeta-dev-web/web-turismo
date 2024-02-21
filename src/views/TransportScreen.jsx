dd4e08c7221543c75ac77d7d9726250bb173d7f3
zeta-dev <nvreparaciones@gmail.com>
2024-02-21 13:39:09 -0300

    se corrige imagen de horario de transporte

--- src/views/TransportScreen.jsx
+++ src/views/TransportScreen.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "../css/TransportScreen.css"
+import horarios from "../assets/horarios.jpg"
 
 const TransportScreen = () => {
   return (
     <div className='text-center mt-2 mb-4'>
       <h2 className='mb-2'>Horarios de empresa San Pedro de Colalao</h2>
-      <img className="img-responsive" src="https://scontent.ftuc1-1.fna.fbcdn.net/v/t39.30808-6/420546623_804969958309686_3083158758093820302_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=GiSbgrJYXL4AX8bKm1S&_nc_ht=scontent.ftuc1-1.fna&oh=00_AfDo8eUXwVo59MqGQSWElMqMJv_coFR52sKqiuXHExN9kg&oe=65D16204" alt="Horarios de empresa San Pedro de Colalao" />
+      <img className="img-responsive" src={horarios} alt="Horarios de empresa San Pedro de Colalao" />
     </div>
   )
 }
