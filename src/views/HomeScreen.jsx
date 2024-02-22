3985aa5bad70d4900646b55e023fe48425d646a9
zeta-dev <nvreparaciones@gmail.com>
2024-02-22 12:50:31 -0300

    se agrega efecto a la carga de las tarjetas

--- src/views/HomeScreen.jsx
+++ src/views/HomeScreen.jsx
@@ -1,4 +1,5 @@
 import { react, useEffect, useState } from "react";
+import { useSpring, animated } from '@react-spring/web'
 import CarouselApp from "../components/CarouselApp";
 import CardApp from "../components/CardApp";
 import "../css/HomeScreen.css"
@@ -17,9 +18,26 @@ const HomeScreen = () => {
     { titulo: 'Eventos Locales', contenido: 'Conoce las fiestas locales...', icono: <TfiAgenda size={35}/>,boton:"Ver más...",ruta:"/eventoslocales" },
     { titulo: 'Ubicación', contenido: 'Como llegar a Trancas...', icono: <SiGooglemaps size={35}/>,boton:"Abrir en Google Maps",ruta:"https://maps.app.goo.gl/YCVdmd4VJHox8xyC6" },
   ];
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  const props = useSpring({
+    opacity: scrollY / 100, // La opacidad del componente aumenta a medida que se desplaza
+  });
+
   return (
     <div> 
       <CarouselApp />
+      <animated.div style={props}>
       <div className="contenedor p-0 ps-3 pe-3 m-0">
   <Row xs={1} md={4} className="g-2 mt-1 justify-content-between flex-wrap">
     {cardsData.map((card, idx) => (
@@ -34,8 +52,8 @@ const HomeScreen = () => {
       </Col>
     ))}
   </Row>
-</div>   <div className="mt-4 mb-4"><CardTextApp/></div>
-        </div>
+</div> <div className="mt-4 mb-4"><CardTextApp/></div>
+</animated.div>  </div>
   );
 };
 export default HomeScreen;
