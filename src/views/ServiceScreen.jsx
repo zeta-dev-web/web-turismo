e7bd78e82836809f4e18bf4cf9f883778d2aae03
zeta-dev <nvreparaciones@gmail.com>
2024-02-22 12:35:24 -0300

    se modifica boton

--- src/views/ServiceScreen.jsx
+++ src/views/ServiceScreen.jsx
@@ -10,8 +10,8 @@ import seguridad from '../assets/seguridad.webp'
 
 const ServiceScreen = () => {
   const cardsData = [
-    { titulo: 'Bomberos', contenido: "Por emergencias llamar al 0381 569-2310", boton:"Ver Ubicación", ruta: "https://maps.app.goo.gl/Dzy5RKZha2xX7Qw6A", imagen: bombero},
-    { titulo: 'Policia', contenido: 'Por emergencias llamar al 0381 438-8140', boton:"Ver Ubicación", ruta: "https://maps.app.goo.gl/JTXkq9AAWnxuSNeP9", imagen: policia,},
+    { titulo: 'Bomberos', contenido: "Por emergencias llamar al 0381 569-2310", boton:"Ver Ubicación", ruta: "https://maps.app.goo.gl/JTXkq9AAWnxuSNeP9", imagen: bombero},
+    { titulo: 'Policia', contenido: 'Por emergencias llamar al 0381 438-8140', boton:"Ver Ubicación", ruta: "https://maps.app.goo.gl/Dzy5RKZha2xX7Qw6A", imagen: policia,},
     { titulo: 'Seguridad Municipal', contenido: 'Por emergencias llamar al 0381 568-1778', boton:"Ver Ubicación", ruta: "https://maps.app.goo.gl/SAZB9FdyTmmLMQRUA", imagen: seguridad},
     { titulo: 'Hospital', contenido: 'Por emergencias llamar al 107', imagen: medico, boton:"Ver Ubicación", ruta:"https://maps.app.goo.gl/xv6GoT7gFBB8mpkt7" } 
   ];
