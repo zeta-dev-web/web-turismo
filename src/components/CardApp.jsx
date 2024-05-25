d9f9943edcb603ebb3cd3015784191383e908bf4
zeta-dev <nvreparaciones@gmail.com>
2024-05-25 12:35:34 -0300

    se edita color de iconos en tarjeta

--- src/components/CardApp.jsx
+++ src/components/CardApp.jsx
@@ -7,9 +7,9 @@ const CardApp =({titulo, contenido, icono, boton, ruta})=>{
 
 
 return(
-<div>
+<div className='custom-font'>
 <Card  border="sucess" className='mt-2 p-0 m-0 cardcolor'>
-      <div className="text-center mt-2">{icono}</div>
+      <div className="text-center mt-2 color-icon">{icono}</div>
       <Card.Body className='text-center card-body'>
         <Card.Title className='fw-bold'>{titulo}</Card.Title>
         <Card.Text>
